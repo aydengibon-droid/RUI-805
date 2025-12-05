910e199327829ea2b58b0601d1993de6b2862e7b
aydengibon-droid <aydengibon@gmail.com>
2025-12-05 14:28:45 -0400

    rev 5

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 className="text-6xl font-bold text-neutral-50">Explore the World</h1>
+      {/* HERO SECTION */}
+      <div className="bg-[url(https://wallpapers.com/images/featured/nature-2ygv7ssy2k0lxlzu.jpg)] w-screen h-screen flex flex-col justify-center items-center">
+        <h1 className="text-6xl font-bold text-neutral-100">Explore the World</h1>
         <p className="text-xl italic">Your travel destiny, written in the stars</p>
-      </div>
 
-      {/* FIRST HALF */}
-      <div className="flex flex-col justify-items">
+      {/* FIRST HALF */}      
+      <div className="flex flex-col justify-center items-center gap-1 mt-10">
         <h1 className="text-xl font-bold text-purple-400">
           Get 40% OFF on your first trip
         </h1>
-        <p className="text-xs text-purple-500">
+        <p className="text-xs text-purple-500"> 
           Join our community and be the first to know about the latest news,offers,and exciting content.
         </p>
       </div>
 
       {/* SECOND HALF */}
-      <div className="flex flex-col justify-items">
+      <div className="flex flex-col justify-center items-center gap-1 mt-10">
         <h1 className="text-4xl font-bold text-blue-900">
           Book a trip with us
         </h1>
@@ -28,12 +28,7 @@ function App() {
           Are you ready to transform your life,achieve your goals, and make a positive change? It all start with taking action. Embrace the opportunity to seize your future now.
         </p>
       </div>
-
-      {/* THIRD HALF */}
-      <div>
-        <img src="https://cdn.wallpapersafari.com/72/36/ncOaDB.jpg" alt="" />
-      </div>
-
+       </div>
     </>
   )
 }

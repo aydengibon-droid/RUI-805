83b59ef0382b5dcf281063a27437caff546f1190
aydengibon-droid <aydengibon@gmail.com>
2025-12-05 12:44:50 -0400

    rev 4

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ function App() {
 
   return (
     <>
+      <div>
+        <h1 className="text-6xl font-bold text-neutral-50">Explore the World</h1>
+        <p className="text-xl italic">Your travel destiny, written in the stars</p>
+      </div>
+
+      {/* FIRST HALF */}
       <div className="flex flex-col justify-items">
         <h1 className="text-xl font-bold text-purple-400">
           Get 40% OFF on your first trip
@@ -13,6 +19,7 @@ function App() {
         </p>
       </div>
 
+      {/* SECOND HALF */}
       <div className="flex flex-col justify-items">
         <h1 className="text-4xl font-bold text-blue-900">
           Book a trip with us
@@ -21,7 +28,12 @@ function App() {
           Are you ready to transform your life,achieve your goals, and make a positive change? It all start with taking action. Embrace the opportunity to seize your future now.
         </p>
       </div>
-      
+
+      {/* THIRD HALF */}
+      <div>
+        <img src="https://cdn.wallpapersafari.com/72/36/ncOaDB.jpg" alt="" />
+      </div>
+
     </>
   )
 }

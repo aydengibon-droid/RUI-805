963df85a5ad668f1d2620c503e66156aa4dceab5
aydengibon-droid <aydengibon@gmail.com>
2025-12-05 12:02:51 -0400

    rev 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold">
-          Get 40% OFF on your
+      <div className="flex flex-col justify-items">
+        <h1 className="text-xl font-bold text-purple-400">
+          Get 40% OFF on your first trip
         </h1>
+        <p className="text-xs text-purple-500">
+          Join our community and be the first to know about the latest news,offers,and exciting content.
+        </p>
+      </div>
+
+      <div className="flex flex-col justify-items">
+        <h1 className="text-4xl font-bold text-blue-900">
+          Book a trip with us
+        </h1>
+        <p className="text-base text-blue-500">
+          Are you ready to transform your life,achieve your goals, and make a positive change? It all start with taking action. Embrace the opportunity to seize your future now.
+        </p>
       </div>
       
     </>
